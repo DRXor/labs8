a2852194f8baaa2e6d3e1635907b4207f474ce2d
Дарья Хорькова <horkovadasa843@gmail.com>
2024-10-05 21:58:39 +0300

    add task

--- task2.pas
+++
@@ -1,8 +0,0 @@
-﻿program task2;
-begin
-  var year := ReadInteger('Введите год:');
-  if (year mod 100 = 0) and (year mod 400 <> 0) then
-    Println('Год не високосный!')
-  else if (year mod 4 = 0) then
-    Println('Год високосный!');  
-end.
