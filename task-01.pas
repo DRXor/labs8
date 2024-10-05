aaebfdf58f92940c28c2dabef4ca2cf847494dfd
Дарья Хорькова <horkovadasa843@gmail.com>
2024-10-05 06:33:47 +0300

    add task-01 again

--- task-01.pas
+++ task-01.pas
@@ -1,4 +1,11 @@
 ﻿program task1;
 begin
-  var hour := ReadInteger('Введите, который сейчас час');
+  var hour := ReadInteger('Введите, который сейчас час:');
+  Assert(hour in 0..23);
+  case hour of
+    4..10 :Println('Доброе утро, мир!');
+    11..16 :Println('Добрый день, мир!');
+    17..22 :Println('Добрый вечер, мир!');
+    else Println('Доброй ночи, мир!');
+  end;
 end.
